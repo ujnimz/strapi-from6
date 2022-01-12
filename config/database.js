d71d67dcce4f77a62700bdbf858f3bcb69183996
Ujith Nimantha <ujithnimantha@gmail.com>
2022-01-12 12:23:45 +0300

    ready to deploy to Google Cloud

--- config/database.js
+++ config/database.js
@@ -1,11 +1,28 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        socketPath: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+      },
+      options: {},
     },
-    useNullAsDefault: true,
   },
 });
