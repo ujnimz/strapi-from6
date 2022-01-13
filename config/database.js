393ca15057a4a0a4401146ffdd075fb045f3689b
Ujith Nimantha <ujithnimantha@gmail.com>
2022-01-13 14:12:40 +0300

    back to default

--- config/database.js
+++ config/database.js
@@ -1,24 +1,28 @@
-// const path = require('path');
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
-    connector: "bookshelf",
+    client: "sqlite",
     connection: {
-      socketPath: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
+    useNullAsDefault: true,
   },
 });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connector: "bookshelf",
+//     connection: {
+//       socketPath: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+//       database: env("DATABASE_NAME"),
+//       user: env("DATABASE_USERNAME"),
+//       password: env("DATABASE_PASSWORD"),
+//     },
+//   },
+// });
