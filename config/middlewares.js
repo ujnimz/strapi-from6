ccb2c66faf4fa5c0fb14b618bb4ae568932d6d78
Ujith Nimantha <ujithnimantha@gmail.com>
2022-01-31 15:16:01 +0300

    disabled google cloud

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,20 +8,20 @@ module.exports = [
   "strapi::body",
   "strapi::favicon",
   "strapi::public",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "fromsix.imgix.net"],
-          "media-src": ["'self'", "data:", "blob:", "fromsix.imgix.net"],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  // {
+  //   name: "strapi::security",
+  //   config: {
+  //     contentSecurityPolicy: {
+  //       useDefaults: true,
+  //       directives: {
+  //         "connect-src": ["'self'", "https:"],
+  //         "img-src": ["'self'", "data:", "blob:", "fromsix.imgix.net"],
+  //         "media-src": ["'self'", "data:", "blob:", "fromsix.imgix.net"],
+  //         upgradeInsecureRequests: null,
+  //       },
+  //     },
+  //   },
+  // },
 ];
 
 // ***** Uncomment above code to use Google Cloud Storage ******
