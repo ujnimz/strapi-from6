885a754b5f8101aa70a56dd348f9afcc239f38cb
Ujith Nimantha <ujithnimantha@gmail.com>
2022-01-31 14:07:10 +0300

    plugins updated

--- config/plugins.js
+++ config/plugins.js
@@ -3,22 +3,23 @@ module.exports = ({ env }) => ({
     config: {
       provider: "strapi-provider-upload-google-cloud-storage",
       providerOptions: {
-        bucketName: env.GCS_BUCKETNAME,
+        bucketName: process.env.GCS_BUCKETNAME,
         publicFiles: true,
         uniform: false,
         serviceAccount: {
           type: "service_account",
-          project_id: env.GCS_PROJECT_ID,
-          private_key_id: env.GCS_PRIVATE_KEY_ID,
-          private_key: env.GCS_PRIVATE_KEY.replace(/\\n/g, "\n"), // KEY
-          client_email: env.GCS_CLIENT_EMAIL,
-          client_id: env.GCS_CLIENT_ID,
-          auth_uri: env.GCS_AUTH_URI,
-          token_uri: env.GCS_TOKEN_URI,
-          auth_provider_x509_cert_url: env.GCS_AUTH_PROVIDER_X509_CERT_URL,
-          client_x509_cert_url: env.GCS_CLIENT_X509_CERT_URL,
+          project_id: process.env.GCS_PROJECT_ID,
+          private_key_id: process.env.GCS_PRIVATE_KEY_ID,
+          private_key: process.env.GCS_PRIVATE_KEY.replace(/\\n/g, "\n"), // KEY
+          client_email: process.env.GCS_CLIENT_EMAIL,
+          client_id: process.env.GCS_CLIENT_ID,
+          auth_uri: process.env.GCS_AUTH_URI,
+          token_uri: process.env.GCS_TOKEN_URI,
+          auth_provider_x509_cert_url:
+            process.env.GCS_AUTH_PROVIDER_X509_CERT_URL,
+          client_x509_cert_url: process.env.GCS_CLIENT_X509_CERT_URL,
         }, // Add your service Account data
-        baseUrl: env.GCS_BASEURL,
+        baseUrl: process.env.GCS_BASEURL,
         basePath: "uploads",
       },
     },
